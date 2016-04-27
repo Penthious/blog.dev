18374915189bfbd23a8b153f1a66019230961ced
Tomas Leffew <tleffew1994@gmail.com>
2016-04-27 12:51:55 -0500

    edited email

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'tleffew1994@gmail.com', 'name' => 'Tomas'),
 
 	/*
 	|--------------------------------------------------------------------------

384c79adfbf241c25d9c19e37a4c9bdb20c21831
Penthious <tleffew1994@gmail.com>
2016-05-10 19:37:39 -0500

    added capstone

--- app/views/projects/portfolio.blade.php
+++ app/views/projects/portfolio.blade.php
@@ -11,9 +11,9 @@
             <div class="row">
                 <div class="portfolioItem">
                     <div class="col-md-12 portfolio-item mainProject">
-                        <a href=""><img class="portfolioItem" src="/img/placeholder.png" alt=""></a>
-                        <h3><a href="#">capstone</a></h3>
-                        <p>capstone</p>
+                        <a href="http://sweetyamsorganic.com"><img class="portfolioItem" src="/img/sweetyams.png" alt=""></a>
+                        <h3><a href="http://sweetyamsorganic.com">Sweetyams</a></h3>
+                        <p>Sweetyams was a project built for the buisness sweetyams as they wanted a online presence, me and my team ended up building the site out of all custom sass and used a robust back end to handle our many differnt menu items. We ended up have a few polymorphic relationships to link the menu items to the coresponding add on.</p>
                     </div>
                 </div>
             </div>
